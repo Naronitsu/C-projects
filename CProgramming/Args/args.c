d8a5870cc67f77042694559c65d7551af9360b90
Naronitsu <bramakiller@gmail.com>
2025-10-17 12:46:50 +0200

    changes

--- CProgramming/Args/args.c
+++ CProgramming/Args/args.c
@@ -14,7 +14,7 @@ THIS WORKS ONLY WITH MAIN.
 int main(int argc, char* argv[]){
 
     for (int i = 0; i < argc; i++) {
-        printf("Argument %d: %s\n", i, argv[i]);
+        printf("Argument %d has value: %s\n", i, argv[i]);
     }
 
     return 0;
