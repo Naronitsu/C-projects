cead23ccdc76593449b6b4853bae01072a539652
Naronitsu <bramakiller@gmail.com>
2025-10-17 12:44:31 +0200

    Adding Command Line Arguments lecture files

---
+++ CProgramming/Args/args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+This program demonstrates command-line argument handling.
+Parameters should not be named differently.
+    argc - number of arguments (Name of the program is also counted)
+    argv - array of dynamic strings - stores name of program, and the rest of the arguments
+
+THIS WORKS ONLY WITH MAIN.
+*/
+
+
+// This will accept CLAs and print them out
+int main(int argc, char* argv[]){
+
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
